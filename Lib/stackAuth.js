c93e906200b2089c24f7cd411c2d4629c7129d56
historyboxworld-dotcom <historyboxworld@gmail.com>
2025-10-21 12:49:27 +0500

    Create stackAuth.js

---
+++ Lib/stackAuth.js
@@ -0,0 +1,7 @@
+import { Stack } from 'stack-auth'
+
+export const stack = new Stack({
+  project_id: '7521f17c-fa2b-4843-b9a1-ec618890c529',
+  publishable_client_key: 'pck_8baysnfzjpvx8ztdsd98m0xtz9kq8wz9f0qq077dfszp0',
+  secret_server_key: 'ssk_z8jn8zkjvkrcp5qqpc3eawcjrsk4tyye04acw1z78h1pg'
+})
