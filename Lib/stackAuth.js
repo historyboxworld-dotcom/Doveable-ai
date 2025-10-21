673f2f77d7423f847ea89fa38cd7c4fe41439585
historyboxworld-dotcom <historyboxworld@gmail.com>
2025-10-21 13:07:34 +0500

    Update stackAuth.js

--- Lib/stackAuth.js
+++ Lib/stackAuth.js
@@ -1,7 +1,13 @@
 import { Stack } from 'stack-auth'
 
-export const stack = new Stack({
-  project_id: '7521f17c-fa2b-4843-b9a1-ec618890c529',
-  publishable_client_key: 'pck_8baysnfzjpvx8ztdsd98m0xtz9kq8wz9f0qq077dfszp0',
-  secret_server_key: 'ssk_z8jn8zkjvkrcp5qqpc3eawcjrsk4tyye04acw1z78h1pg'
-})
+let stack = null
+
+// Initialize only on client side
+if (typeof window !== 'undefined') {
+  stack = new Stack({
+    project_id: '7521f17c-fa2b-4843-b9a1-ec618890c529',
+    publishable_client_key: 'pck_8baysnfzjpvx8ztdsd98m0xtz9kq8wz9f0qq077dfszp0',
+  })
+}
+
+export { stack }
